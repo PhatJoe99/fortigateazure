a6fe0ff4a4d7c428fe3f63e3b9fab68fb4314c14
PhatJoe99 <karol.dybowski@mtisystems.pl>
2021-11-24 10:13:00 +0100

    Dodanie sprawdzenie aktualności JSONa

--- script.py
+++ script.py
@@ -36,8 +36,10 @@ def deleteOldJson():                        #usuwanie starego JSONa
     listDir = os.listdir('.')
     for item in listDir:
         if item.endswith(".json"):
+            oldJson = item
             os.remove(item)
             print('File removed ' + item)
+    return(item)
 
 
 deleteOldJson()
@@ -127,35 +129,41 @@ def jsonImport(AzurePart, jsonFileName):
 
 
 def main():
-    deleteOldJson()                                             #Usunięcie starego JSONa
+    oldJson = deleteOldJson()                                   #Usunięcie starego JSONa
     jsonFileName = downloadJson()                               #Pobranie nowego JSONa
-    fortek = {                                                  #Dane logowania do fortka
-    'host':settings.IP,
-    'username':settings.LOGIN,
-    'password':settings.PASS,
-    'device_type':'fortinet'
-    }
-    net_connect = Netmiko(**fortek)                             #Nawiązanie połączenia z fortkiem
-    for itemAzure in Azure:                                     #Wylistowanie wszystkich adresów z danego regionu Azure
-        ipAddresses = jsonImport(itemAzure, jsonFileName)
-        command_list = []                                       #Pusta lista komend do puszczenia na fortka
-        command_list.append('config firewall address')
-        for item in ipAddresses:
-            command_list.extend(ipPushToFortigate(itemAzure, item))
-        command_list.append('end')
-        print(command_list)
-        send_config = net_connect.send_config_set(command_list) #Puszczenie listy komend na fortka
-        command_list = []                                       #Wyczyszczenie listy komend
-        command_list.append('config firewall addrgrp')
-        command_list.append('edit "' + itemAzure + '"')
-        members = ""
-        for item in ipAddresses:
-            members += ipAddToSubnet(itemAzure, item)
-        command_list.append('set member' + members)
-        command_list.append('next')
-        command_list.append('end')
-        send_config = net_connect.send_config_set(command_list)
-        command_list = []
+    if oldJson != jsonFileName:
+        print("Przystępuję od aktualizacji konfiguracji na fortigate")
+        fortek = {                                                  #Dane logowania do fortka
+        'host':settings.IP,
+        'username':settings.LOGIN,
+        'password':settings.PASS,
+        'device_type':'fortinet'
+        }
+        net_connect = Netmiko(**fortek)                             #Nawiązanie połączenia z fortkiem
+        for itemAzure in Azure:                                     #Wylistowanie wszystkich adresów z danego regionu Azure
+            ipAddresses = jsonImport(itemAzure, jsonFileName)
+            command_list = []                                       #Pusta lista komend do puszczenia na fortka
+            command_list.append('config firewall address')
+            for item in ipAddresses:
+                command_list.extend(ipPushToFortigate(itemAzure, item))
+            command_list.append('end')
+            print(command_list)
+            send_config = net_connect.send_config_set(command_list) #Puszczenie listy komend na fortka
+            print(send_config)
+            command_list = []                                       #Wyczyszczenie listy komend
+            command_list.append('config firewall addrgrp')
+            command_list.append('edit "' + itemAzure + '"')
+            members = ""
+            for item in ipAddresses:
+                members += ipAddToSubnet(itemAzure, item)
+            command_list.append('set member' + members)
+            command_list.append('next')
+            command_list.append('end')
+            send_config = net_connect.send_config_set(command_list)
+            print(send_config)
+            command_list = []
+    else:
+        print("JSON nie uległ zmianie, konfiguracja fortigate nadal aktualna")
 
 if __name__ == "__main__":                                      #Wykonanie skryptu
     main()
